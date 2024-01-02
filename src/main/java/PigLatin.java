978d645a8ae6c249a8d360985629c905404a3d0b
Natnael Berhe <natnaelberhe1225@gmail.com>
2024-01-02 07:28:09 +0000

    d

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,9 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        if (in == null || in.length() == 0){
+            return "";
+        }
+        return in.substring(1) + in.charAt(0) + "ay";
     }
 }
